731599c4b331deabeba2fb5a7339a52df27c3938
Steve <steve@trigram.ai>
2024-07-02 10:08:27 -0500

    read and write csv prototype

--- step1/src/main.cpp
+++ step1/src/main.cpp
@@ -3,6 +3,8 @@
 #include<algorithm>
 #include<memory>
 #include<functional>
+#include<fstream>
+#include<sstream>
 #include "job.h"
 #include "programmer.h"
 #include "pilot.h"
@@ -14,6 +16,30 @@ bool IsInBounds(T value, T lower, T upper) {
 
 int ContainsTheString(const std::function<bool(const std::string&)>& test, const std::vector<std::string>& collection) {
     return std::count_if(collection.begin(), collection.end(), test);
+}
+
+void writeExampleCSV(const std::string& filename) {
+    std::ofstream file(filename);
+    file << "id,type,name\n";
+    file << "0,bird,blue\n";
+    file << "1,cat,felix\n";
+    file << "2,dog,toto\n";
+    file.close();
+}
+
+std::vector<std::vector<std::string>> readCSV(const std::string& filename) {
+    std::ifstream file(filename);
+    std::vector<std::vector<std::string>> data;
+    std::string line;
+    while (std::getline(file, line)) {
+        std::istringstream s(line);
+        std::string field;
+        data.emplace_back();
+        while (getline(s, field, ',')) {
+            data.back().push_back(field);
+        }
+    }
+    return data;
 }
 
 int main()
